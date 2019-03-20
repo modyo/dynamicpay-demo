b1e9fd979913127843247b307b301cf2ec6f3954
Toño Silva Portell <jsilva@modyo.com>
2019-03-20 18:22:28 -0300

    first commit

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Acerca de</h1>
+    <p>Este sitio ha sido generado por el Modyo CLI</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    
+    <h1>Holi, world!</h1>
+   
+    <p>
+      Sitio estático.
+    </p>
+
+    <p class="home-links">
+      <a href="www.modyo.com">Modyo</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
