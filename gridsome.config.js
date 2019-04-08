0afd398e420e283cc67b671f9769835f7a7f2704
Roberto Rivera S <rrivera@modyo.com>
2019-04-08 17:51:53 -0400

    Add the raight favicon

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,11 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Modyo Static Demo Site',
+  siteName: "Modyo Static Demo Site",
+  icon: "src/favicon.png",
   plugins: []
-}
+};
