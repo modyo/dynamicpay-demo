e134d52d5f0dc06ef7eada980be1e5dd85463fd3
Luis <lnazarit@modyo.com>
2019-04-11 17:37:53 -0400

    components

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,9 +1,38 @@
 <template>
-  <ul class="navbar-nav ml-auto my-2 my-lg-0">
-    <li class="nav-item" v-for="edge in $static.menu.edges" :key="edge.node.slug">
-      <g-link class="nav-link js-scroll-trigger" :to="edge.node.slug">{{edge.node.title}}</g-link>
-    </li>
-  </ul>
+  <div>
+    <div class="top-header d-none d-sm-block">
+      <div class="container d-flex">
+
+          <div class="top-header--left">
+            <a href="" class="top-header__link link-left active"><small>Personal</small></a>
+            <a href="" class="top-header__link link-left"><small>Golden</small></a>
+            <a href="" class="top-header__link link-left"><small>Business</small></a>
+          </div>
+          <div class="top-header--right ml-auto">
+            <a href="https://dynamicbank.modyo.cloud/retail/places" class="top-header__link link-right"><small><i class="icon-map-marker"></i> Locations</small></a>
+            <a href="" class="top-header__link link-right"><small>Customer Service</small></a>
+            <a href="" class="top-header__link link-right link-red"><small>Emergencies</small></a>
+          </div>
+
+      </div>
+    </div>
+  <header id="main-header">
+    <div class="container d-flex">
+      <g-link class="navbar-brand js-scroll-trigger" to="/" >
+        <img :alt="$static.metaData.siteName" :title="$static.metaData.siteName" src="https://cloud.modyocdn.com/uploads/d401ed2b-bd7a-4c0a-9b32-e9f1e427bb74/original/dynamic_bank.png" />
+      </g-link>
+      <nav class="navbar ml-auto" id="mainNav">
+        <ul class="nav ml-auto my-2 my-lg-0 d-flex">
+          <li class="nav-item" v-for="edge in $static.menu.edges" :key="edge.node.slug">
+            <g-link class="nav-link js-scroll-trigger" :to="edge.node.slug">{{edge.node.title}}</g-link>
+          </li>
+        </ul>
+        <a href="#" class="btn btn-secondary mr-1">Online Banking</a>
+        <a href="#" class="btn btn-primary">Become a Client</a>
+      </nav>
+    </div>
+  </header>
+  </div>
 </template>
 
 <static-query>
@@ -17,7 +46,13 @@ query Menu {
       }
     }
   }
+  metaData {
+  siteName
+  }
 }
+
+
+
 </static-query>
 
 <script>
@@ -45,4 +80,37 @@ export default {
 </script>
 
 <style scoped>
+  .top-header {
+    background: #f4f4f4;
+    font-size: 12px
+  }
+  .top-header__link.active {
+    font-weight: 400;
+    background-color: #FFF;
+    color: #4d4d4d;
+  }
+  .top-header__link.link-left,
+  .top-header__link.link-right {
+    color: #4d4d4d;
+  }
+  .top-header__link {
+    display: inline-block;
+    padding: 3px 12px;
+  }
+  .navbar-brand {
+    display: flex;
+    align-items: center;
+  }
+  .navbar-brand img {
+    height: 25px;
+    display: block;
+  }
+  #main-header {
+    display: flex;
+    align-items: center;
+    padding: 1rem 0;
+  }
+  .navbar {
+    padding: 0;
+  }
 </style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,50 +1,25 @@
 <template>
-  <div>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-      <div class="container">
-        <g-link class="navbar-brand js-scroll-trigger" to="/">{{ $static.metaData.siteName }}</g-link>
-        <button
-          class="navbar-toggler navbar-toggler-right"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarResponsive"
-          aria-controls="navbarResponsive"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-        <MainMenu></MainMenu>
-        </div>
-      </div>
-    </nav>
+<div>
+    <MainMenu></MainMenu>
     <!-- Site body -->
     <slot/>
     <!-- Footer -->
-    <footer class="bg-light py-5">
-      <div class="container">
-        <div class="small text-center text-muted">Copyright &copy; 2019 - Start Bootstrap</div>
-      </div>
-    </footer>
-  </div>
+    <Footer></Footer>
+</div>
 </template>
 
 <static-query>
-query {
-  metaData {
-    siteName
-  }
-}
+
 </static-query>
 
 <script>
-  import MainMenu from '~/components/menu.vue'
+  import MainMenu from '~/components/menu.vue';
+  import Footer from '~/components/footer.vue'
 
   export default {
     components: {
-      MainMenu
+      MainMenu,
+        Footer
     }
   }
 </script>
