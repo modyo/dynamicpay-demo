cb6c9090c95624c153c5fdc3612834b4070e0b11
Roberto Rivera S <rrivera@modyo.com>
2019-05-03 18:30:25 -0400

    Fix warning

--- src/components/card-carousel.vue
+++ src/components/card-carousel.vue
@@ -1,47 +1,51 @@
 <template>
-<div class="container my-5">
-  <carousel :perPageCustom="[[768, 2], [1024, 3]]">
-    <slide v-for="entry in entries">
-      <div class="p-3 h-100">
-        <CardItemCarousel :title="entry.fields.title" :description="entry.fields.description" :cover="entry.fields.cover.url"></CardItemCarousel>
-      </div>
-    </slide>
-  </carousel>
-</div>
+  <div class="container my-5">
+    <carousel :perPageCustom="[[768, 2], [1024, 3]]">
+      <slide v-for="entry in entries" :key="entry.id">
+        <div class="p-3 h-100">
+          <CardItemCarousel
+            :title="entry.fields.title"
+            :description="entry.fields.description"
+            :cover="entry.fields.cover.url"
+          ></CardItemCarousel>
+        </div>
+      </slide>
+    </carousel>
+  </div>
 </template>
 
 <script>
-    import CardItemCarousel from "./card-item-carousel";
-    import axios from "axios";
-    import { Carousel, Slide } from 'vue-carousel';
-    export default {
-        components: {
-            CardItemCarousel,
-            Carousel,
-            Slide
-        },
-        name: "card-carousel",
-        data() {
-            return {
-                loading: true,
-                entries: ''
-            }
-        },
-        created() {
-            const baseURI = "https://dynamicbank.modyo.build/api/content/spaces/fintech/content_types/card/entries/?meta.tag=carousel-home";
-            axios
-                .get(baseURI)
-                .then(result => {
-                    this.entries = result.data.entries;
-                    this.loading = false;
-                })
-                .catch(error => {
-                    // console.log(error.response);
-                    this.error = error.response;
-                });
-        }
-
-    }
+import CardItemCarousel from "./card-item-carousel";
+import axios from "axios";
+import { Carousel, Slide } from "vue-carousel";
+export default {
+  components: {
+    CardItemCarousel,
+    Carousel,
+    Slide
+  },
+  name: "card-carousel",
+  data() {
+    return {
+      loading: true,
+      entries: ""
+    };
+  },
+  created() {
+    const baseURI =
+      "https://dynamicbank.modyo.build/api/content/spaces/fintech/content_types/card/entries/?meta.tag=carousel-home";
+    axios
+      .get(baseURI)
+      .then(result => {
+        this.entries = result.data.entries;
+        this.loading = false;
+      })
+      .catch(error => {
+        // console.log(error.response);
+        this.error = error.response;
+      });
+  }
+};
 </script>
 
 <style>
