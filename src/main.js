8646f10972763b20127091b4270917eb444b49fe
Roberto Rivera S <rrivera@modyo.com>
2019-05-03 11:32:34 -0400

    Format

--- src/main.js
+++ src/main.js
@@ -6,15 +6,13 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import "~/assets/base.css";
 import DefaultLayout from "~/layouts/Default.vue";
 
-export default function(Vue, { head }) {
+export default function(Vue, { head, router }) {
   // Set default layout as a global component
   Vue.prototype.$api = "https://dynamicbank.modyo.build/api/content/spaces";
   Vue.component("Layout", DefaultLayout);
-  head.link.push(
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css?family=Montserrat:300,400,700"
-    }
-  );
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css?family=Montserrat:300,400,700"
+  });
   Vue.use(BootstrapVue);
 }
