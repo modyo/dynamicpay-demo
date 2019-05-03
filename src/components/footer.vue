cf0772649c4318a8c616facfeeeb5447ab5b9416
Luis <lnazarit@modyo.com>
2019-05-03 15:59:43 -0400

    footer menu filtrado

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,17 @@
 <template>
   <footer class="bg-light py-5">
-    <div class="container">
-      <div class="small text-center text-muted" v-html="hint"></div>
+    <div class="container align-items-center d-flex justify-content-center flex-column">
+      <ul class="nav d-inline-flex mb-5">
+        <li class="nav-item" v-for="item in menu" :key="item.url">
+          <g-link class="nav-link js-scroll-trigger" :to="item.url">{{item.name}}</g-link>
+        </li>
+      </ul>
+      <div class="row">
+        <div class="col-lg-3"></div>
+        <div class="col-lg-6">
+          <div class="small text-center text-muted" v-html="hint"></div>
+        </div>
+      </div>
     </div>
   </footer>
 
@@ -13,7 +23,8 @@
         name: "Footer",
         data() {
             return {
-                hint: ""
+                hint: "",
+                menu: []
             };
         },
         mounted() {
@@ -32,7 +43,41 @@
                     // console.log(error.response);
                     this.error = error.response;
                 });
+            this.fetchMenu();
 
+        },
+        methods: {
+            fetchMenu: function() {
+                this.error = this.menu = [];
+                this.loading = true;
+                const baseURI =
+                    "https://dynamicbank.modyo.build/api/content/spaces/static-data/content_types/menu-item/entries";
+                axios
+                    .get(baseURI)
+                    .then(result => {
+                        // console.log("result.data.entries: ", result.data.entries);
+                        const entries = result.data.entries;
+                        let menutmp = [];
+                        for (const item of entries) {
+                            // console.log("item: ", item);
+                            const menuItem = {
+                                id: item.meta.uuid,
+                                name: item.fields.name,
+                                url: item.fields.url,
+                                position: item.fields.position,
+                                showFooter: item.fields.show_in_footer
+                            };
+                            menutmp.push(menuItem);
+                        }
+                        this.menu = menutmp.filter(entry => entry.showFooter)
+
+                        this.loading = false;
+                    })
+                    .catch(error => {
+                        // console.log(error.response);
+                        this.error = error.response;
+                    });
+            }
         }
     };
 </script>
