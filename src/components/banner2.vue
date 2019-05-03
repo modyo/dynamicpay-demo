89e7355ab52f161ae79b2447b3c80ac86105ab6a
Luis <lnazarit@modyo.com>
2019-05-03 15:37:50 -0400

    carousel

--- src/components/banner2.vue
+++ src/components/banner2.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="banner-2 py-5">
-    <div class="container p-5">
+    <div class="container p-5" :style="{backgroundImage: 'url(' + entry.cover.url + ')'}">
       <div class="row no-gutters">
         <div class="col-md-8"></div>
         <div class="col-md-4">
           <div class="bg-white p-5">
-          <h2>¡Descuentos todos los días!</h2>
+          <h2>{{ entry.title}}</h2>
           <p>Conoce los beneficios del mes de Abril</p>
           <div class="mt-4">
-            <a href="#" class="btn btn-secondary">Ver más</a>
+            <a :href="entry.url" class="btn btn-secondary">{{ entry.button}}</a>
           </div>
         </div>
         </div>
@@ -19,14 +19,35 @@
 
 
 <script>
+    import axios from "axios";
     export default {
-        name: "banner2"
+        name: "banner2",
+        data() {
+            return {
+                loading: true,
+                entry: ''
+            }
+        },
+        created() {
+            const baseURI = "https://dynamicbank.modyo.build/api/content/spaces/fintech/content_types/card/entries/7b6e7b55-1583-4dc3-8106-06b3296403b3";
+            axios
+                .get(baseURI)
+                .then(result => {
+                    this.entry = result.data.fields;
+                    this.loading = false;
+                })
+                .catch(error => {
+                    // console.log(error.response);
+                    this.error = error.response;
+                });
+        }
+
+
     }
 </script>
 
 <style scoped>
   .banner-2 .container {
-    background: url("https://images.ctfassets.net/p6eyia4djstu/1a0p0Rrnh2CCgacAUWCK00/8b7dbcdd10daa27658f774e58471e13b/IMAGEN-RESTAURANT-BCO.jpg");
     background-size: cover;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap-vue"
+import VueCarousel from 'vue-carousel';
 import "~/assets/base.css";
 import DefaultLayout from "~/layouts/Default.vue";
+
 
 export default function(Vue, { head }) {
   // Set default layout as a global component
