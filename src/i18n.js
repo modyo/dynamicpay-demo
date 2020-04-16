2716c65fb76fa9b0bdefa5d82292ac52fbf88aa6
hassennachar <hnachar@gmail.com>
2020-04-16 18:41:22 -0400

    language Detector

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,10 @@
 import i18n from "i18next";
+import detector from "i18next-browser-languagedetector";
 import { reactI18nextModule } from "react-i18next";
 
 import translationEN from './locales/en';
 import translationES from './locales/es';
+
 
 // the translations
 const resources = {
@@ -14,11 +16,41 @@ const resources = {
     },
 };
 
+const languagedetectorOptions = {
+    // order and from where user language should be detected
+    order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+
+    // keys or params to lookup language from
+    lookupQuerystring: 'lng',
+    lookupCookie: 'i18next',
+    lookupLocalStorage: 'i18nextLng',
+    lookupFromPathIndex: 0,
+    lookupFromSubdomainIndex: 0,
+
+    // cache user language on
+    caches: ['localStorage', 'cookie'],
+    excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+    // optional expire and domain for set cookie
+    cookieMinutes: 10,
+    cookieDomain: 'myDomain',
+
+    // optional htmlTag with lang attribute, the default is:
+    htmlTag: document.documentElement,
+
+    // only detect languages that are in the whitelist
+    checkWhitelist: true,
+    // optional set cookie options, reference:[MDN Set-Cookie docs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie)
+    cookieOptions: {path:'/'}
+};
+
 i18n
+    .use(detector)
     .use(reactI18nextModule) // passes i18n down to react-i18next
     .init({
         resources,
-        lng: "en",
+        fallbackLng: "en", // use en if detected lng is not available
+        detection: languagedetectorOptions,
 
         keySeparator: false, // we do not use keys in form messages.welcome
 
