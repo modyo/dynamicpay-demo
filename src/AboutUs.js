2bd99cf1336c33a52ece98047b7f4259c245abf9
hassennachar <hnachar@gmail.com>
2020-04-21 12:14:31 -0400

    about us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from "react";
 import getClient from "./modyoClient";
+import getEntries from "./modyoBankyoEntries";
 import Loading from "./Loading";
 import "./Plans.css";
 import img1 from "./img-stock/bg-23.jpg";
 import { withNamespaces } from 'react-i18next';
+import i18n from "./i18n";
 
 class AboutUs extends Component {
   constructor(props) {
@@ -15,9 +17,12 @@ class AboutUs extends Component {
   }
 
   componentDidMount() {
-    const client = getClient();
-    const clientCard = client.getContentType("fintech", "card");
-    clientCard.getEntries("meta.tag=hero-about")
+    this.getCard();
+    this.checkLanguageChanged();
+  }
+
+  getCard() {
+    getEntries("getdynamicpay-content", "card", i18n.language, "meta.tag=hero-about")
       .then(data => {
         let items = [];
         for (let index = 0; index < data.entries.length; index++) {
@@ -29,9 +34,17 @@ class AboutUs extends Component {
       });
   }
 
+  checkLanguageChanged() {
+    i18n.on('languageChanged', (lng) => {
+      this.setState({ hero: null });
+      this.getCard();
+    });
+  }
+
   layoutHero() {
     const hero = this.state.hero[0];
     const { t } = this.props;
+
     return (
       <Fragment>
         <div className="header-video white">
@@ -68,9 +81,11 @@ class AboutUs extends Component {
     );
   }
   render() {
+    const { t } = this.props;
+    const loading = t('global-loading');
     return (
       <Fragment>
-        {this.state.hero ? this.layoutHero() : <Loading title="Cargando..." />}
+        {this.state.hero ? this.layoutHero() : <Loading title={loading} />}
       </Fragment>
     );
   }
