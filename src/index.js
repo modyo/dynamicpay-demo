e83042d05843a4270d4efb73e42798f004b6bdf4
Johann Rojas <jrojas@modyo.com>
2021-04-09 16:59:19 -0500

    Fixed non-closed meta tags and SW updated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './i18n';
 import App from './App';
-// import * as serviceWorker from './swRegister';
+import * as serviceWorker from './swRegister';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.register();
+serviceWorker.register();
