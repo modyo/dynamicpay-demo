b0c77a6e80d902cd84e1a2720f977c59efc5e617
Johann Rojas <jrojas@modyo.com>
2021-04-13 13:22:27 -0500

    feat: Service worker now serving data from cached files

--- public/swCustom.js
+++ public/swCustom.js
@@ -2,21 +2,21 @@
 const cacheName = 'v1';
 
 const cacheAssets = [
-  
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+  'https://cdn.materialdesignicons.com/3.8.95/css/materialdesignicons.min.css',
+  'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js'
 ];
 
 // Call Install Event
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
   e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
+    caches.open(cacheName).then(cache => {
         console.log('Service Worker: Caching Files');
         cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
+    }).then(() => self.skipWaiting())
+  )
 });
 
 // Call Activate Event
@@ -29,8 +29,8 @@ self.addEventListener('activate', e => {
         cacheNames.map(cache => {
           if (cache !== cacheName) {
             console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
-          }
+            return caches.delete(cache)
+           }
         })
       );
     })
@@ -39,6 +39,35 @@ self.addEventListener('activate', e => {
 
 // Call Fetch Event
 self.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching');
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  if(e.request.url.indexOf('https') === 0){
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+      caches.match(e.request).then(response => {
+        // If the request is in the cache
+        if(response){
+          console.log(`  %cService Worker found in cache ${e.request.url}`, "color: #28a745");
+          return response;
+        }
+        
+        // If the request is NOT in the cache, fetch and cache
+        var reqClone = e.request.clone();
+        //Fetching
+        return fetch(reqClone).then( res => {
+          if ( !res ) {
+            console.log("%cService Worker no response from fetch ", "color: #7d030f")
+            return res;
+          }
+          //Caching 
+          caches.open(cacheName).then(cache => {
+            cache.put(e.request, res.clone())
+            console.log('%cService Worker new data cached ' + e.request.url, "color:#ffc107");
+            return res
+          })
+        }).catch( err => {
+          console.log('Service Worker Error fetching and caching', err);
+        })
+      })
+    )
+  }
 });
+    
